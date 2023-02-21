93b03143a6c5646901f44dc0a873e16e101dad19
Uday Shankar <112121903+shankaruday108@users.noreply.github.com>
2023-02-21 15:40:37 +0530

    List of respected former Prime Ministers of India
    List of respected former Prime Ministers of India

--- Selenium/Assignments/Web Scraping - Assignment 2/List of respected former Prime Ministers of India/List of respected former Prime Ministers of India.py
+++ Selenium/Assignments/Web Scraping - Assignment 2/List of respected former Prime Ministers of India/List of respected former Prime Ministers of India.py
@@ -9,7 +9,7 @@
 # 3. Then click on the List of all Prime Ministers of India
 # 4. Then scrap the mentioned data and make theDataFrame.
 
-# In[7]:
+# In[1]:
 
 
 import selenium as sm
@@ -22,23 +22,24 @@ from selenium.webdriver.common.by import By
 import time
 
 
-# In[11]:
+# In[2]:
 
 
 # Now connection to the driver which is available in this folder of my laptop
 driver = webdriver.Chrome(r"D:\Uday Shankar's Data\Softwares\chromedriver_win32\chromedriver.exe")
+time.sleep(3)
 
 
-# In[12]:
+# In[3]:
 
 
 # Now opening the naukri.com website in a new window
 driver.get("https://www.jagranjosh.com/")
-driver.fullscreen_window()
+# driver.fullscreen_window()
 time.sleep(3)
 
 
-# In[13]:
+# In[4]:
 
 
 gk_search = driver.find_element(By.XPATH, "/html/body/div/div[1]/div/div[1]/div/div[6]/div/div[1]/header/div[3]/ul/li[9]/a")
@@ -46,41 +47,158 @@ gk_search.click()
 time.sleep(3)
 
 
-# In[17]:
+# In[6]:
 
 
-pm_search = driver.find_element(By.XPATH, "/html/body/div[1]/div/div/div[2]/div/div[14]/div/div/ul/li[3]/a")
+pm_search = driver.find_element(By.XPATH, "/html/body/div[1]/div/div/div[2]/div/div[14]/div/div/ul/li[4]")
 pm_search.click()
 time.sleep(3)
 
 
-# In[24]:
+# In[7]:
 
 
-pm_name = driver.find_element(By.XPATH, "/html/body/div[1]/div[2]/div/div[2]/div/div[1]/div/div/div/div[4]/span/div[2]/table/tbody/tr[6]/td[2]/p/strong/a")
+pm_names = driver.find_element(By.XPATH, "//div[@class='table-box']/table/tbody/tr/td[2]/p")
+pm_names.text
 
 
-# In[27]:
+# In[34]:
 
 
-start = 0
-end = 10
+pm_office = driver.find_element(By.XPATH, "//div[@class='table-box']/table/tbody/tr/td[4]/p")
+pm_office.text
+
+
+# In[44]:
+
+
+button_start = 0
+button_end = 1
+
 names = []
 born_deads = []
 term_of_offices = []
 remarks = []
+years_months = []
+
+pmnames= ""
+born= ""
+term= ""
+remk= ""
+pm_years= ""
+
+time.sleep(5)
+
+for getdata in range(button_start,button_end):
+    
+    pmnames  = driver.find_elements(By.XPATH, "//div[@class='table-box']/table/tbody/tr/td[2]/p")
+    time.sleep(3)
+    
+    born  = driver.find_elements(By.XPATH, "//div[@class='table-box']/table/tbody/tr/td[3]/p")
+    time.sleep(3)
+    
+    term  = driver.find_elements(By.XPATH, "//div[@class='table-box']/table/tbody/tr/td[4]/p[1]")
+    time.sleep(3)
+    
+    pm_years  = driver.find_elements(By.XPATH, "//div[@class='table-box']/table/tbody/tr/td[4]/p[2]")
+    time.sleep(3)
+    
+    remk  = driver.find_elements(By.XPATH, "//div[@class='table-box']/table/tbody/tr/td[5]/p")
+    time.sleep(3)
+    
+#     pm_years = driver.find_elements(By.XPATH, "//div[@class='table-box']/table/tbody/tr/td[4]/p[2]")
+#     time.sleep(3)
+    
+    if len(pmnames) > 0:
+     
+        for a  in pmnames:
+            names.append(a.text)
+
+        for b  in born:
+            born_deads.append(b.text)
+
+        for c  in term:
+            term_of_offices.append(c.text)
+            
+        for e  in pm_years:
+            years_months.append(e.text)   
+                
+        for d  in remk:
+            remarks.append(d.text)
+
+        time.sleep(2)
+
+    else:
+        break
+        
+time.sleep(3)   
 
 
-# In[ ]:
+# In[45]:
 
 
-# No class name only a tag
+names
 
 
-# In[ ]:
+# In[46]:
 
 
+len(names)
 
+
+# In[47]:
+
+
+born_deads
+
+
+# In[48]:
+
+
+len(born_deads)
+
+
+# In[49]:
+
+
+term_of_offices
+
+
+# In[50]:
+
+
+len(term_of_offices)
+
+
+# In[58]:
+
+
+# years_months
+
+
+# In[52]:
+
+
+len(years_months)
+
+
+# In[53]:
+
+
+remarks
+
+
+# In[54]:
+
+
+len(remarks)
+
+
+# In[59]:
+
+
+all_data = pd.DataFrame({'Name':names[:18], 'Born-Dead':born_deads[:18], 'Term of Office':term_of_offices[:18], 'Remark':remarks[:18], 'Remark':remarks[:18]})
+all_data
 
 
 # In[ ]:
